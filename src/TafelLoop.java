cc3fc54fcc7e367f5d5b3c0ddac34f51a13fdef3
CorinneSpruit <corinnemeijdam@hotmail.com>
2020-09-27 21:10:23 +0200

    Hallo, Ik liep even vast op de loop, omdat ik het boogje vergat te openen, daardoor wilde het programma de i niet pakken. Bij de 2e tafel moet een nieuwe loop gemaakt worden, anders komt de vraag ook in een loop en blijft het programma nieuwe cijfers vragen (echter gaf het programma niet de volledige tafel) En bij de vraag naar een nieuw getal, moet een nieuwe variabele aangemaakt worden, anders geeft het programma direct 2x de tafel van hetzelfde getal. Groet Corinne

--- src/TafelLoop.java
+++ src/TafelLoop.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class TafelLoop {
+    public static void main (String [] arg){
+
+        System.out.println("Hallo, van welk getal wil je de tafel weten?");
+
+        var s = new Scanner(System.in);
+
+                var getalx = s.nextInt();
+
+        System.out.println("Dat is een mooi getal! De tafel van "+getalx+" is:");
+
+                for (int i=1;i<=10;i++) {
+
+                    System.out.println(i + " x " + getalx + " = " + getalx * i);
+                }
+
+        System.out.println();
+
+    System.out.println("Laten we er nog een doen! Noem nog eens een getal");
+
+                   var getaly = s.nextInt();
+
+                    System.out.println("Nog zo'n mooi getal! Houd je vast, hier komt de tafel van " + getaly);
+
+        for (int i=1;i<=10;i++) {
+            System.out.println(i + " x " + getaly + " = " + getaly * i);
+
+        }
+
+    }
 }
